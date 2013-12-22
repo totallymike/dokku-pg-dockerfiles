6e208cbafb400ad18476c8129a69e9bd26fcf20b
Michael Westbom <michael.westbom@alphasights.com>
2013-12-22 16:26:03 -0500

    Update to 9.2

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,12 @@ MAINTAINER	kload "kload@kload.fr"
 # prevent apt from starting postgres right after the installation
 RUN	echo "#!/bin/sh\nexit 101" > /usr/sbin/policy-rc.d; chmod +x /usr/sbin/policy-rc.d
 
+RUN echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+
+RUN wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4Cf8.asc | apt-key add -
 RUN apt-get update
-RUN	LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get install -y -q postgresql-9.1 postgresql-contrib-9.1
+
+RUN	LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get install -y -q postgresql-9.2
 RUN rm -rf /var/lib/apt/lists/*
 RUN apt-get clean
 
@@ -16,5 +20,5 @@ RUN	rm /usr/sbin/policy-rc.d
 
 ADD	. /usr/bin
 RUN	chmod +x /usr/bin/start_pgsql.sh
-RUN echo 'host all all 0.0.0.0/0 md5' >> /etc/postgresql/9.1/main/pg_hba.conf
-RUN sed -i -e"s/var\/lib/opt/g" /etc/postgresql/9.1/main/postgresql.conf
+RUN echo 'host all all 0.0.0.0/0 md5' >> /etc/postgresql/9.2/main/pg_hba.conf
+RUN sed -i -e"s/var\/lib/opt/g" /etc/postgresql/9.2/main/postgresql.conf
--- start_pgsql.sh
+++ start_pgsql.sh
@@ -4,7 +4,7 @@ if [[ ! -z "$1" ]]; then
     mkdir -p /opt/postgresql
     cp -a /var/lib/postgresql/* /opt/postgresql/
     chown -R postgres:postgres /opt/postgresql
-    su postgres sh -c "/usr/lib/postgresql/9.1/bin/postgres --single  -D  /var/lib/postgresql/9.1/main  -c config_file=/etc/postgresql/9.1/main/postgresql.conf" <<< "CREATE USER root WITH SUPERUSER PASSWORD '$1';"
-    su postgres sh -c "/usr/lib/postgresql/9.1/bin/postgres --single  -D  /var/lib/postgresql/9.1/main  -c config_file=/etc/postgresql/9.1/main/postgresql.conf" <<< "CREATE DATABASE db;"
+    su postgres sh -c "/usr/lib/postgresql/9.2/bin/postgres --single  -D  /var/lib/postgresql/9.2/main  -c config_file=/etc/postgresql/9.2/main/postgresql.conf" <<< "CREATE USER root WITH SUPERUSER PASSWORD '$1';"
+    su postgres sh -c "/usr/lib/postgresql/9.2/bin/postgres --single  -D  /var/lib/postgresql/9.2/main  -c config_file=/etc/postgresql/9.2/main/postgresql.conf" <<< "CREATE DATABASE db;"
 fi
-su postgres sh -c "/usr/lib/postgresql/9.1/bin/postgres           -D  /var/lib/postgresql/9.1/main  -c config_file=/etc/postgresql/9.1/main/postgresql.conf  -c listen_addresses=*"
+su postgres sh -c "/usr/lib/postgresql/9.2/bin/postgres           -D  /var/lib/postgresql/9.2/main  -c config_file=/etc/postgresql/9.2/main/postgresql.conf  -c listen_addresses=*"
